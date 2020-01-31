6afeacabca7b4cce7f98cb68dcab9720dcd707b4
mutazalaa <mutazalaasalah@gmail.com>
2020-01-31 15:10:16 +0200

    blogsStateless

--- Front/src/components/blogsComponents/BlogsFullPage.js
+++ Front/src/components/blogsComponents/BlogsFullPage.js
@@ -1,19 +1,17 @@
-import React, { Component } from "react";
+import React from "react";
 import Navbar from './../sharedComponents/Navbar';
 import Header from "./Header";
 import Blogs from './Blogs';
 import Footer from './../sharedComponents/Footer';
-class BlogsFullPage extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <Navbar />
-        <Header />
-        <Blogs />
-        <Footer />
-      </React.Fragment>
-    );
-  }
+const BlogsFullPage =()=>{
+  return (
+    <React.Fragment>
+      <Navbar />
+      <Header />
+      <Blogs />
+      <Footer />
+    </React.Fragment>
+  );
 }
 
 export default BlogsFullPage;
